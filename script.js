2e4785c319ed706207a7cae239f57b37a6d5cb26
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-11-30 18:54:31 +0100

    removed optimized scroll

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-;(function() {
-  var throttle = function(type, name, obj) {
-    var obj = obj || window;
-    var running = false;
-    var func = function() {
-        if (running) { return; }
-        running = true;
-        requestAnimationFrame(function() {
-            obj.dispatchEvent(new CustomEvent(name));
-            running = false;
-        });
-    };
-    obj.addEventListener(type, func);
-  };
-  throttle ("scroll", "optimizedScroll");
-})();
-
 var logo = document.getElementById("logo");
     
-window.addEventListener("optimizedScroll", function() {
+window.addEventListener("scroll", function() {
   logo.style.transform = "rotate("+(window.pageYOffset/2)+"deg)";
 });
