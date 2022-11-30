440c210ab7c1332afdaa093ada18b9a495c9c7e4
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-11-30 18:33:20 +0100

    optimized scroll

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+;(function() {
+  var throttle = function(type, name, obj) {
+    var obj = obj || window;
+    var running = false;
+    var func = function() {
+        if (running) { return; }
+        running = true;
+        requestAnimationFrame(function() {
+            obj.dispatchEvent(new CustomEvent(name));
+            running = false;
+        });
+    };
+    obj.addEventListener(type, func);
+  };
+  throttle ("scroll", "optimizedScroll");
+})();
+
 var logo = document.getElementById("logo");
     
-window.addEventListener("scroll", function() {
+window.addEventListener("optimizedScroll", function() {
   logo.style.transform = "rotate("+(window.pageYOffset/2)+"deg)";
 });
